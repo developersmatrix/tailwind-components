e80892dad1919fffb6b16f33ee9d915437b3f6d5
neo <developersmatrix3x3@gmail.com>
2022-11-10 22:15:47 +0530

    working on navbar mobile

--- js/index.js
+++ js/index.js
@@ -1,17 +1,14 @@
-const navList = document.getElementById('nav-list');
+const navList = document.getElementById("nav-list");
 
-const menuBtn = document.getElementById('menu-open');
-const closeBtn = document.getElementById('menu-close');
-
-
+const menuBtn = document.getElementById("menu-open");
+const closeBtn = document.getElementById("menu-close");
 
 const menuHandler = (event) => {
-    navList.classList.toggle('hidden')
-    menuBtn.classList.toggle('hidden')
-    closeBtn.classList.toggle('hidden')
-}
+  navList.classList.toggle("hidden");
+  //   navList.classList.toggle("translate-x-full");
+  menuBtn.classList.toggle("hidden");
+  closeBtn.classList.toggle("hidden");
+};
 
-menuBtn.addEventListener('click', menuHandler)
-closeBtn.addEventListener('click', menuHandler)
-
-
+menuBtn.addEventListener("click", menuHandler);
+closeBtn.addEventListener("click", menuHandler);
