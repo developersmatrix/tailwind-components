cca982a3fda6a5d2ed784570019b34ddabf1d27f
neo <developersmatrix3x3@gmail.com>
2022-11-18 23:03:50 +0530

    color scheme sync

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "844px",
+      bm: "660px",
+    },
+    colors: {
+      transparent: "transparent",
+      matte: "#22253B",
+      whit: "#eeeeee",
+    },
   },
   plugins: [],
 };
